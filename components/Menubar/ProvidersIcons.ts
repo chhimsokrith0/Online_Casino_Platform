462b8023a83d40be5012628f001e2d77520e2833
chhimsokrith <sokrithchhim@gmail.com>
2024-12-02 14:48:45 +0700

    f

--- components/Menubar/ProvidersIcons.ts
+++ components/Menubar/ProvidersIcons.ts
@@ -1,3 +1,4 @@
+// Importing icons
 import icon_PG from "@/assets/icon_Providers/PG.svg";
 import icon_Joker from "@/assets/icon_Providers/Joker.svg";
 import icon_JILI from "@/assets/icon_Providers/JILI.svg";
@@ -7,7 +8,8 @@ import icon_Pegasus from "@/assets/icon_Providers/Pegasus.svg";
 import icon_DreamGaming from "@/assets/icon_Providers/DreamGaming.svg";
 import icon_Worldmatch from "@/assets/icon_Providers/Worldmatch.svg";
 import icon_WMCASINO from "@/assets/icon_Providers/WMCASINO.svg";
-import icon_Amigo from "@/assets/icon_Providers/Amigo.svg";
+// Import placeholder for missing icons
+import placeholderIcon from "@/assets/icon_Providers/download.png"; // Fallback icon
 import icon_Naga from "@/assets/icon_Providers/Naga.svg";
 import icon_Nolimit from "@/assets/icon_Providers/Nolimit.svg";
 import icon_SA from "@/assets/icon_Providers/SA.svg";
@@ -29,48 +31,44 @@ import icon_FACHAI from "@/assets/icon_Providers/FACHAI.svg";
 import icon_FastSpin from "@/assets/icon_Providers/FastSpin.svg";
 import icon_CQ9 from "@/assets/icon_Providers/CQ9.svg";
 import icon_Hacksaw from "@/assets/icon_Providers/Hacksaw.svg";
-import icon_RoyalSlot from "@/assets/icon_Providers/RoyalSlot.svg"
+import icon_RoyalSlot from "@/assets/icon_Providers/RoyalSlot.svg";
 import icon_Winfinity from "@/assets/icon_Providers/Winfinity.svg";
 
+// Updated array of provider icons
 const ProvidersIcons = [
   { name: "PGSoft", icon: icon_PG },
   { name: "Joker", icon: icon_Joker },
-  { name: "PGSoft", icon: icon_PG },
-    { name: "Joker", icon: icon_Joker },
-    { name: "JILI", icon: icon_JILI },
-    { name: "PragmaticPlay", icon: icon_PragmaticPlay },
-    { name: "Habanero", icon: icon_Habanero },
-    { name: "Pegasus", icon: icon_Pegasus },
-    { name: "DreamGaming", icon: icon_DreamGaming },
-    { name: "Worldmatch", icon: icon_Worldmatch },
-    { name: "WMCASINO", icon: icon_WMCASINO },
-    { name: "Amigo", icon: icon_Amigo },
-    { name: "Naga", icon: icon_Naga },
-    { name: "Nolimit City", icon: icon_Nolimit },
-    { name: "SA", icon: icon_SA },
-    { name: "Ae Sexy", icon: icon_AeSexy },
-    { name: "Evoplay", icon: icon_Evoplay },
-    { name: "Spribe", icon: icon_Spribe },
-    { name: "Sbobet", icon: icon_Sbobet },
-    { name: "Evolution Gaming", icon: icon_Evolution },
-    { name: "Mancala", icon: icon_Mancala },
-    { name: "Play`n Go", icon: icon_PlaynGo },
-    { name: "Relax Gaming", icon: icon_Relax },
-    { name: "Smartsoft Gaming", icon: icon_Smartsoft },
-    { name: "3 Oaks Gaming", icon: icon_3Oaks },
-    { name: "Dragongaming", icon: icon_Dragongaming },
-    { name: "We Gaming", icon: icon_WeGaming },
-    { name: "Rich88", icon: icon_Rich88 },
-    { name: "SpadeGaming", icon: icon_SpadeGaming },
-    { name: "FA CHAI", icon: icon_FACHAI },
-    { name: "FastSpin", icon: icon_FastSpin },
-    { name: "CQ9", icon: icon_CQ9 },
-    { name: "Hacksaw", icon: icon_Hacksaw },
-    { name: "Royal Slot Gaming", icon: icon_RoyalSlot },
-    { name: "Winfinity", icon: icon_Winfinity },
+  { name: "JILI", icon: icon_JILI },
+  { name: "PragmaticPlay", icon: icon_PragmaticPlay },
+  { name: "Habanero", icon: icon_Habanero },
+  { name: "Pegasus", icon: icon_Pegasus },
+  { name: "DreamGaming", icon: icon_DreamGaming },
+  { name: "Worldmatch", icon: icon_Worldmatch },
+  { name: "WMCASINO", icon: icon_WMCASINO },
+  { name: "Amigo", icon: placeholderIcon }, // Placeholder for missing Amigo.svg
+  { name: "Naga", icon: icon_Naga },
+  { name: "Nolimit City", icon: icon_Nolimit },
+  { name: "SA", icon: icon_SA },
+  { name: "Ae Sexy", icon: icon_AeSexy },
+  { name: "Evoplay", icon: icon_Evoplay },
+  { name: "Spribe", icon: icon_Spribe },
+  { name: "Sbobet", icon: icon_Sbobet },
+  { name: "Evolution Gaming", icon: icon_Evolution },
+  { name: "Mancala", icon: icon_Mancala },
+  { name: "Play`n Go", icon: icon_PlaynGo },
+  { name: "Relax Gaming", icon: icon_Relax },
+  { name: "Smartsoft Gaming", icon: icon_Smartsoft },
+  { name: "3 Oaks Gaming", icon: icon_3Oaks },
+  { name: "Dragongaming", icon: icon_Dragongaming },
+  { name: "We Gaming", icon: icon_WeGaming },
+  { name: "Rich88", icon: icon_Rich88 },
+  { name: "SpadeGaming", icon: icon_SpadeGaming },
+  { name: "FA CHAI", icon: icon_FACHAI },
+  { name: "FastSpin", icon: icon_FastSpin },
+  { name: "CQ9", icon: icon_CQ9 },
+  { name: "Hacksaw", icon: icon_Hacksaw },
+  { name: "Royal Slot Gaming", icon: icon_RoyalSlot },
+  { name: "Winfinity", icon: icon_Winfinity },
 ];
 
 export default ProvidersIcons;
-
-
-
