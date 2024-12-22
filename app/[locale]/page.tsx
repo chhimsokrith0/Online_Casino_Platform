11ecf0da86bb7a2f0c5bca4c0d0a9c4225be10e0
chhimsokrith <sokrithchhim@gmail.com>
2024-12-22 14:34:22 +0700

    f

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,23 +1,3 @@
-// import { getMessages } from "next-intl/server";
-// import HomeContent from "@/components/HomeContent";
-
-// export async function generateMetadata({
-//   params: { locale },
-// }: {
-//   params: { locale: string };
-// }) {
-//   const messages: any = await getMessages({ locale });
-//   const title = messages.NavbarLinks.homeTitle;
-
-//   return {
-//     title,
-//   };
-// }
-
-// export default function Home({ params: { locale } }: { params: { locale: string } }) {
-//   return <HomeContent locale={locale} />;
-// }
-
 
 import { getMessages } from "next-intl/server";
 import HomeContent from "@/components/HomeContent";
