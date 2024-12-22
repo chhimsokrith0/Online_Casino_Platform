58bb10489c5a339151d386c1e4cb41982e7d39dc
chhimsokrith <sokrithchhim@gmail.com>
2024-12-22 14:40:33 +0700

    f

--- i18n.ts
+++ i18n.ts
@@ -1,38 +1,38 @@
-// import { notFound } from "next/navigation";
-// import { getRequestConfig } from "next-intl/server";
-
-// const locales = ["en", "th", "hi", "zh", "kh", "id", "ms", "sg", "vi", "ph", "mm", "lo", "bn", "ko","tw", "ur", "hk", "pt", "au", "ca"];
-
-// export default getRequestConfig(async ({ locale }) => {
-//   if (!locales.includes(locale as any)) notFound();
-
-//   return {
-//     messages: (await import(`./messages/${locale}.json`)).default,
-    
-//   };
-// });
-
-
-
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
-const locales = ["en", "th", "hi", "zh", "kh", "id", "ms", "sg", "vi", "ph", "mm", "lo", "bn", "ko", "tw", "ur", "hk", "pt", "au", "ca"];
+const locales = ["en", "th", "hi", "zh", "kh", "id", "ms", "sg", "vi", "ph", "mm", "lo", "bn", "ko","tw", "ur", "hk", "pt", "au", "ca"];
 
 export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as string)) {
-    notFound(); // Redirect to the `not-found.tsx` page
-  }
+  if (!locales.includes(locale as any)) notFound();
 
-  try {
-    return {
-      messages: (await import(`./messages/${locale}.json`)).default,
-    };
-  } catch (error) {
-    console.error("Error loading messages:", error);
-    notFound(); // Redirect if the locale file cannot be loaded
-  }
+  return {
+    messages: (await import(`./messages/${locale}.json`)).default,
+    
+  };
 });
+
+
+
+// import { notFound } from "next/navigation";
+// import { getRequestConfig } from "next-intl/server";
+
+// const locales = ["en", "th", "hi", "zh", "kh", "id", "ms", "sg", "vi", "ph", "mm", "lo", "bn", "ko", "tw", "ur", "hk", "pt", "au", "ca"];
+
+// export default getRequestConfig(async ({ locale }) => {
+//   if (!locales.includes(locale as string)) {
+//     notFound(); // Redirect to the `not-found.tsx` page
+//   }
+
+//   try {
+//     return {
+//       messages: (await import(`./messages/${locale}.json`)).default,
+//     };
+//   } catch (error) {
+//     console.error("Error loading messages:", error);
+//     notFound(); // Redirect if the locale file cannot be loaded
+//   }
+// });
 
 
 
